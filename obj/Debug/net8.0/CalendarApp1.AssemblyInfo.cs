ad165d8f1e96f97c9480afe136cd3dee071252e1
Michal <micharegister@gmail.com>
2024-04-17 23:29:28 +0200

    dodane szczegoly eventow

--- obj/Debug/net8.0/CalendarApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/CalendarApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalendarApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfa07e9d4b41b8ab7ba091caf4266db28d78fb72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7b1787b9e80b128e0ece90c8a04ccbecf479e22")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalendarApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalendarApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
