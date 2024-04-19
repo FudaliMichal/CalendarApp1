9ac17a5eb1a83cb9df7e6aaf03d1973a5ec873c4
Michal <micharegister@gmail.com>
2024-04-19 07:33:38 +0200

    offcanvas z event info

--- obj/Debug/net8.0/CalendarApp1.AssemblyInfo.cs
+++ obj/Debug/net8.0/CalendarApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalendarApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7b1787b9e80b128e0ece90c8a04ccbecf479e22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad165d8f1e96f97c9480afe136cd3dee071252e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalendarApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalendarApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
